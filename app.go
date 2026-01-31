013de462faf0a0665d1a02079789576de1699df1
iaasif <imtiazasif.cse@gmail.com>
2026-01-31 16:38:53 +0600

    refactor: update investment calculation and improve formatting

--- app.go
+++ app.go
@@ -1,14 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	// fmt.Println("Hello, World!")
 	const inflationRate = 0.07
 	var inverstmentAmount float64
-	years := 10
-	expectedReturn := 0.10
+	years := 10.0
+	expectedReturnrate := 5.5
 
 	fmt.Print("investment Amount: ")
 	fmt.Scanln(&inverstmentAmount)
+
+	futureValue := inverstmentAmount * math.Pow(1+expectedReturnrate/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate, years)
+
+	fmt.Println("Future Value:", futureValue)
+	fmt.Println("Future Real Value:", futureRealValue)
 }
