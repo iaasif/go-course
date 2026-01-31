db0e716f91765c187b3f60fe7c590723e6426242
iaasif <imtiazasif.cse@gmail.com>
2026-01-31 16:30:31 +0600

    initialize Go module and create main application file

---
+++ app.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// fmt.Println("Hello, World!")
+	const inflationRate = 0.07
+	var inverstmentAmount float64
+	years := 10
+	expectedReturn := 0.10
+
+	fmt.Print("investment Amount: ")
+	fmt.Scanln(&inverstmentAmount)
+}
