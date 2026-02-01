664a3771d571a3cd447003f0d15833e09454930c
iaasif <imtiazasif.cse@gmail.com>
2026-02-01 23:30:08 +0600

    .

--- app.go
+++ app.go
@@ -9,11 +9,13 @@ func main() {
 	// fmt.Println("Hello, World!")
 	const inflationRate = 0.07
 	var inverstmentAmount float64
-	years := 10.0
-	expectedReturnrate := 5.5
+	var years float64
+	var expectedReturnrate float64
 
 	fmt.Print("investment Amount: ")
 	fmt.Scanln(&inverstmentAmount)
+	fmt.Print("years?: ")
+	fmt.Scanln(&years)
 
 	futureValue := inverstmentAmount * math.Pow(1+expectedReturnrate/100, years)
 	futureRealValue := futureValue / math.Pow(1+inflationRate, years)
