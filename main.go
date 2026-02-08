fe86584deb5c5de783d2895ed9d941d6ac73fdff
iaasif <imtiazasif.cse@gmail.com>
2026-02-09 00:51:03 +0600

    feat: Add `getUserInput` function with input validation and integrate it into `main` to prompt for note title and content.

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"fmt"
+)
 
+func main() {
+	getUserInput("note title")
+	getUserInput("note content")
+}
+
+func getUserInput(prompt string) (string, error) {
+	fmt.Println(prompt)
+	var value string
+	fmt.Scan(&value)
+	if value == "" {
+		// fmt.Println("Please enter a value")
+		return "", errors.New("you should put some value here ")
+	}
+	return value, nil
 }
