c55abaa699dce90d5a3e02d6500c8edf2c812eaa
iaasif <imtiazasif.cse@gmail.com>
2026-02-14 12:55:37 +0600

    working fine

--- note/note.go
+++ note/note.go
@@ -10,18 +10,18 @@ import (
 )
 
 type Note struct {
-	title     string
-	content   string
-	createdAt time.Time
+	Title     string
+	Content   string
+	CreatedAt time.Time
 }
 
 func (note Note) DisplayNote() {
-	fmt.Printf("Title is: %v and The content is: \n\n%v\n\n", note.title, note.content)
+	fmt.Printf("Title is: %v and The content is: \n\n%v\n\n", note.Title, note.Content)
 }
 
 func (note Note) Save() error {
-	fileName := strings.ReplaceAll(note.title, " ", "_")
-	fileName = strings.ToLower(fileName)
+	fileName := strings.ReplaceAll(note.Title, " ", "_")
+	fileName = strings.ToLower(fileName) + ".json"
 	json, err := json.Marshal(note)
 
 	if err != nil {
@@ -36,8 +36,8 @@ func New(title, content string) (Note, error) {
 		return Note{}, errors.New("Invalid input.")
 	}
 	return Note{
-		title:     title,
-		content:   content,
-		createdAt: time.Now(),
+		Title:     title,
+		Content:   content,
+		CreatedAt: time.Now(),
 	}, nil
 }
