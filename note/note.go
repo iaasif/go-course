26165f52b937f0edf15b6ebaec60c6f3e49ee63d
iaasif <imtiazasif.cse@gmail.com>
2026-02-14 13:08:38 +0600

    update working

--- note/note.go
+++ note/note.go
@@ -11,7 +11,7 @@ import (
 
 // struct tag
 type Note struct {
-	Title     string    `"title"`
+	Title     string    `json:"title"`
 	Content   string    `json:"content"`
 	CreatedAt time.Time `json:"created_at"`
 }
