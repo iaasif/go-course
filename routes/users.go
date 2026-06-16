2f6cb710e18bef24e1fa7e6b1dbd5fd67beffab1
iaasif <imtiazasif.cse@gmail.com>
2026-06-16 19:31:17 +0600

    login working with token

--- routes/users.go
+++ routes/users.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"app/models"
+	"app/utils"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -28,19 +29,22 @@ func signup(context *gin.Context) {
 func login(context *gin.Context) {
 	var user models.User
 	err := context.ShouldBindJSON(&user)
-
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"message": "could not parse request data"})
 		return
 	}
 
 	err = user.ValidateCredentials()
-
 	if err != nil {
 		context.JSON(http.StatusUnauthorized, gin.H{"message": "could not authenticate user"})
 		return
 	}
 
-	context.JSON(http.StatusOK, gin.H{"message": "Login successful!"})
+	token, err := utils.GenerateToken(user.Email, user.ID)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "authenticate user."})
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Login successful!", "token": token})
 
 }
